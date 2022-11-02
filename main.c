970108723147de668cd0794a0a186dbf8a863d40
YT4l0 <ytalodias206@gmail.com>
2022-11-02 15:04:47 -0300

    code upate

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <windows.h>
+#include "passagens.c"
 
-#define TITULO_OPCAO1 "Soma de dois numeros inteiros"
-#define TITULO_OPCAO2 "Multiplicacao de dois numeros inteiros"
-#define TITULO_OPCAO3 "Divisao de dois numeros inteiros"
-#define TITULO_OPCAO4 "Saida do programa"
+#define TITULO_OPCAO1 "Realizar reserva"
+#define TITULO_OPCAO2 "Excluir reserva"
+#define TITULO_OPCAO3 "listar reservas"
+#define TITULO_OPCAO4 "Buscar reservas"
+#define TITULO_OPCAO5 "consultar vagas disponiveis"
+#define TITULO_OPCAO6 "consultar quantidade de passageiros"
+#define TITULO_OPCAO7 "sair"
 
-#define N_OPCOES 4
+#define N_OPCOES 7
+
 #define OPCAO1 '1'
 #define OPCAO2 '2'
 #define OPCAO3 '3'
 #define OPCAO4 '4'
+#define OPCAO5 '5'
+#define OPCAO6 '6'
+#define OPCAO7 '7'
 
 /****
 * Função: LimpaBuffer()
@@ -51,24 +59,8 @@ int LeOpcao(int menorValor, int maiorValor) {
     }
     return op;
 }
-/****
-* Função: LeInteiro()
-* Descrição: Lê um número inteiro introduzido pelo usuário
-* Parâmetros: Nenhum
-* Retorno: O inteiro lido
-****/
-int LeInteiro(void) {
-    int umInt, nValoresLidos;
-    printf("Digite um valor inteiro: ");
-    nValoresLidos = scanf("%d", &umInt);
-    while (nValoresLidos == 0) { /*Nenhum inteiro foi lido*/
-        LimpaBuffer();
-        printf("Entrada incorreta. Digite um valor inteiro: ");
-        nValoresLidos = scanf("%d", &umInt);
-    }
-    LimpaBuffer();
-    return umInt;
-}
+
+
 /****
 * Função: ApresentaMenu
 * Descrição: Apresenta menu com número indeterminado de opções
@@ -93,44 +85,55 @@ void ApresentaMenu(int nItens, int menorOpcao, ...) {
 
 int main(void) {
     unsigned char op;
-    int inteiro1, inteiro2;
+
+    
     unsigned int saida = 0;
 
     do {
         ApresentaMenu(N_OPCOES, OPCAO1,
                       TITULO_OPCAO1, TITULO_OPCAO2,
-                      TITULO_OPCAO3, TITULO_OPCAO4);
+                      TITULO_OPCAO3, TITULO_OPCAO4, TITULO_OPCAO5, TITULO_OPCAO6, TITULO_OPCAO7);
         op = LeOpcao(OPCAO1, OPCAO1 + N_OPCOES - 1);
         switch(op) {
             case OPCAO1:
                 Beep(1000,500); /* Emite um beep */
-                inteiro1 = LeInteiro();
-                inteiro2 = LeInteiro();
-                printf("%d + %d = %d\n", inteiro1, inteiro2,
-                       inteiro1 + inteiro2);
+                Passagem*passagem = reserva_passagem();
+                
                 break;
 
             case OPCAO2:
                 Beep(1000,500);
-                inteiro1 = LeInteiro();
-                inteiro2 = LeInteiro();
-                printf("%d * %d = %d\n", inteiro1, inteiro2,
-                       inteiro1 * inteiro2);
+                //add funtion clear
                 break;
 
             case OPCAO3:
                 Beep(1000,500);
-                inteiro1 = LeInteiro();
-                inteiro2 = LeInteiro();
-                printf("%d / %d = %d\n", inteiro1, inteiro2,
-                       inteiro1 / inteiro2);
+               
                 break;
 
             case OPCAO4:
                 Beep(1000,500);
+               
+                break;
+
+            case OPCAO5:
+                Beep(1000,500);
+
+                break;
+            case OPCAO6:
+            Beep(1000,500);
+
+            break;
+
+            case OPCAO7:
+            
+
+             Beep(1000,500);
                 saida = 1;
                 printf("Obrigado por usar este programa.");
-                break;
+
+
+            break;
 
             default:
                 printf("Este programa possui um bug.");
---
+++ passagens.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct passagem Passagem;
+
+Passagem* reserva_passagem(void);
