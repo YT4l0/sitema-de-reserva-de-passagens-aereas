7fb33e1408bbd941c31552a49e09105f4ad3d28c
YT4l0 <ytalodias206@gmail.com>
2022-11-05 13:15:38 -0300

    code update 1.0

--- passagens.c
+++ passagens.c
@@ -9,9 +9,49 @@ struct passagem{
     char destino[50];
     int numero_aviao;
 
+    
+
+
 };
 
+struct No{
+
+    Passagem info;
+
+    no *next; //proximo elemento
+    no  *prior; //elemento anterior
+
+};
+struct Lista{
+
+    Passagem info;
+
+    lista *next;
+    lista *prior; 
+
+}lista;
+
+lista* lista_insere(lista *l, int i){
+    lista *new =(lista*)malloc(sizeof(lista));
+    if(new==NULL){
+        printf("sem espaço");
+        exit(1)
+    }
+
+    new-> info = i;
+    new -> next = l;
+    next -> prior = l;
+
+
+
+return new;
+}
+
 Passagem* reserva_passagem(void){
+
+
+
+
     Passagem* passagem = (Passagem*)malloc(sizeof(Passagem));
     printf("digite o nome:  ");
     scanf(" %[^\n]", passagem->nome);
